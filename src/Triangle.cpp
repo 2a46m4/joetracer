154b904e04c8e409835134f7bc848d9b43964b85
dabanya02 <gcciscool@protonmail.com>
2022-12-30 23:09:41 -0800

    triangle definition

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -1,15 +1,12 @@
 #include "Triangle.h"
 
-Triangle::Triangle(){
-	
-}
+Triangle::Triangle() {}
 
-Triangle::Triangle(const Point &P0, const Point &P1, const Point &P2){
-	
-}
+Triangle::Triangle(const Point &P0, const Point &P1, const Point &P2) {}
 
-bool Triangle::boundingBox(double t0, double t1,
-                           aabb &outputBox) const override;
+bool Triangle::boundingBox(double t0, double t1, aabb &outputBox) const {
+  
+}
 
 bool Triangle::hit(const Ray &r, hitRecord &rec, double tMin,
                    double tMax) const override;
